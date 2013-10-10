c26dbea783c86e72850778eb311196067a9bf7f3
Truong My Hai Ninh <truong.my.hai.ninh@framgia.com>
2013-10-10 17:03:53 +0700

    Finish chapter 8 - Sign in and out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do
+		@user = User.new(name: "Example User", email: "user@example.com",
+			password: "foobar", password_confirmation: "foobar")
+	end
+
+	subject { @user }
+
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
+
+	describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
